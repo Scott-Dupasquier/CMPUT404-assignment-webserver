57a1fc96e7ee868886def2452482b579c5c70c0a
Scott Dupasquier <sdupasqu@cs.ualberta.ca>
2020-01-24 10:36:42 -0700

    All freetests pass

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -32,10 +33,70 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        
+        # self.url = "http://127.0.0.1:8080"
+        split_data = self.data.decode('utf-8').split(' ')
+        method = split_data[0]
+
+        if (len(split_data) > 1):
+            filename = split_data[1] # PROBABLY WILL NEED TO CHANGE
+            # self.url += "/www" + filename
+
+        # Can only handle GET method, not POST/PUT/DELETE
+        if (method != "GET"):
+            self.cannot_handle_method()
+        elif filename == "/":
+            # print("TRUE")
+            # directories = os.listdir('www/')
+            # for d in directories:
+            #     print(d, "\r\n")
+            #     self.request.sendall(bytearray(d, 'utf-8'))
+            self.send_file("www/index.html")
+        else:
+            # self.request.sendall(bytearray("200 OK\r\n",'utf-8'))
+            self.send_file("www" + filename)
+
+    def redirect(new_location):
+        response = "HTTP/1.1 301 Moved Permanently\r\nLocation: " + new_location
+        self.request.send(bytearray(response, 'utf-8'))
+
+    def cannot_handle_method(self):
+        self.request.send(bytearray("HTTP/1.1 405 Method Not Allowed\r\n\r\n", 'utf-8'))
+
+    def send_file(self, filename):
+        # to_send = "Content-Type: text/html\r\n\r\n"
+        to_send = ""
+        try:
+            req_file = open(filename, 'r')
+
+            # if (filename.find('.') != -1):
+            split_data = filename.split('.')
+            content_type = split_data[1]
+
+            try:
+                content_length = os.path.getsize(filename)
+            except:
+                print("Failed to get length")
+                content_length = 0
+            else:
+                print(content_length)
+
+            to_send = "HTTP/1.1 200 OK\nContent-Type: text/" + content_type + "\r\n\r\n" #"\nContent-Length: " + content_length + 
+            self.request.send(bytearray(to_send, 'utf-8'))
+
+            to_send = ""
+            for l in req_file:
+                to_send += l
+
+            self.request.sendall(bytearray(to_send, 'utf-8'))
+            req_file.close()
+        except:
+            self.request.send(bytearray("HTTP/1.1 404 Not Found\r\n\r\n", 'utf-8'))
+            # raise request.HTTPError(url=self.url, code=404, msg="Not Found", hdrs=None, fp=None)
+            
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 8080
+    HOST, PORT = "127.0.0.1", 8080
 
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
